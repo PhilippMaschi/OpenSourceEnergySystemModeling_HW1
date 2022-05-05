84f563d20a5819f32829991e534b3cdf95a228f3
PhilippMaschi <phil.masch@hotmail.com>
2022-05-05 19:56:23 +0200

    Fixed bug where double letters would become one

--- Hangman.py
+++ Hangman.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 def check_word(string: str) -> bool:
     # check if numbers, special chars or spaces are in the string
     if string.isalpha():
@@ -10,32 +13,46 @@ def check_word(string: str) -> bool:
 def ask_user() -> str:
     user_input = input("Type a word that needs to be guessed \n"
                        "don't use space, numbers or special characters: \n")
+    print("\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n")
+    return user_input.lower()
+
+
+def check_user_input(user_input: str) -> str:
+    count = 0
     while True:
+        count += 1
         # check if the word is actually a word:
         if not check_word(user_input):  # if its not a word, ask again
             print("That was not a word, try again")
             user_input = ask_user()
+            # after 3 attempts end the game:
+            if count == 3:
+                print("you are incapable of typing a single word")
+                quit()
         else:  # if it is a word, break the loop
             break
     return user_input.lower()
 
 
-def guess(word: str, word_dict: dict) -> None:
+def guess(word: str, word_list: List[str], check_list: List[bool]) -> None:
     guess = input("Guess one letter: \n").lower()
     # check if guess was correct:
     if guess in word:
-        # update the word_dict
-        word_dict[guess] = True
         print("That is correct, this letter is in the word")
+        # update the check_list
+        for index, letter in enumerate(word_list):
+            if letter == guess:
+                check_list[index] = True
+
     else:
         print("No, this letter is not in the word")
 
 
-def print_word(word_dict: dict) -> None:
+def print_word(word_list: List[str], check_list: List[bool]) -> None:
     # create string with only the letters that already have been guessed
     underscore_string = ""
-    for letter, status in word_dict.items():
-        if status:
+    for index, letter in enumerate(word_list):
+        if check_list[index]:  # if corresponding bool in check list is true, than print the letter
             underscore_string += letter
         else:
             underscore_string += "_"
@@ -44,16 +61,22 @@ def print_word(word_dict: dict) -> None:
 
 def main():
     word = ask_user()
+    # check if user input is correct
+    word = check_user_input(word)
     # create a dictionary that withholds the status of the letters that have been guessed correctly:
-    word_dict = {letter: False for letter in word}
+    word_list = [letter for letter in word]
+    check_list = [False for letter in word]
     # while loop to play the game:
     while True:
         guess(word=word,
-              word_dict=word_dict)
-        print_word(word_dict=word_dict)
+              word_list=word_list,
+              check_list=check_list)
+
+        print_word(word_list=word_list,
+                   check_list=check_list)
 
         # check if the game is over
-        if all(value == True for value in word_dict.values()):
+        if all(value == True for value in check_list):
             print("well done!")
             break
 
